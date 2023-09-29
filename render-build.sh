337ade37af178803fcf97d54c13a07d1718efd2f
darionauta <dariusz.stanek@gmail.com>
2023-09-29 11:59:07 +0200

    debug, wip

--- render-build.sh
+++ render-build.sh
@@ -5,14 +5,13 @@ set -o errexit
 STORAGE_DIR=/opt/render/project/.render
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "Home direcotry: $HOME"
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME
+  cd $HOME/project/
 else
   echo "...Using Chrome from cache"
 fi
